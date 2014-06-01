60de29a2fa5ce41d8da574eff54d5ece113f744e
gba3455 <SQZS@SQZSMacBook-Air.local>
2014-06-01 11:01:55 +0900

    temp

--- pkgtest/Demo.java
+++ pkgtest/Demo.java
@@ -12,15 +12,28 @@ public class Demo {
 	 */
 	public static void main(String[] args) {
 
-		int i = 0;
-
-		int j = 1;
-		System.out.println("hello" + i);
-
-		System.out.println("12");
-		FileInputStream in = new FileInputStream("");
-		Set set = new HashSet();
-		set.add("ad");
+//		int i = 0;
+//
+//		int j = 1;
+//		System.out.println("hello" + i);
+//
+//		System.out.println("12");
+//		FileInputStream in = new FileInputStream("");
+//		Set set = new HashSet();
+//		set.add("ad");
+		
+//		for(int i = 2; i < 1000 ; i++){
+//			int flag = 1;
+//			for(int j = 2; j < i ; j++){
+//					if(i % j == 0){
+//						flag = 0;
+//						break;
+//					}
+//			}
+//			if(flag == 1){
+//				System.out.println(i);
+//			}
+//		}
 	}
 
 }
